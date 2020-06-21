d28b558d51a57f3de3c5a4b524cdc37957189bc0
yasiaan <yzouhri5@gmail.com>
2020-06-21 14:10:19 +0100

    First init gestion

---
+++ src/Controller/SecurityController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+
+class SecurityController extends AbstractController
+{
+    /**
+     * @Route("/", name="security")
+     */
+    public function index()
+    {
+        return $this->render('security/index.html.twig');
+    }
+
+    /**
+     * @Route("/connexion", name="security_login")
+     */
+    public function login()
+    {
+        return $this->render('security/login.html.twig');
+    }
+
+    /**
+     * @Route("/inscription", name="security_registration")
+     */
+    public function registration()
+    {
+
+        return $this->render('security/index.html.twig');
+    }
+}
